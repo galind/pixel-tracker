39f13c43b4aa2a01bcee713808d7dcfd7b0eba2c
galind <guillem.yt1@gmail.com>
2022-08-03 03:52:31 +0200

    File added

---
+++ run.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, Response
+import base64
+import requests
+
+
+app = Flask(__name__)
+
+host = None
+port = None
+webhook_url = None
+
+
+def send_webhook(identifier):
+    if not webhook_url:
+        return
+
+    payload = {
+        'username': 'Pixel Tracker',
+        'content': 'The email with the identifier **{}** has been opened.'.format(identifier)
+    }
+    requests.post(webhook_url, data=payload)
+
+
+@app.route('/pixel.gif')
+def pixel():
+    identifier = request.args.get('identifier')
+    send_webhook(identifier)
+
+    pixel_data = base64.b64decode("R0lGODlhAQABAIAAAP8AAP8AACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==")
+    return Response(pixel_data, mimetype='image/gif')
+
+
+if __name__ == '__main__':
+    app.run(host=host, port=port)
